c53b4a685ddc6507cfb95ff09c83fab1b7268b8c
Odysseus Ribeiro <odysseus.ribeiro@hotmail.com>
2022-03-11 14:13:03 +0100

    Test

--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,8 @@ namespace FormationCMSUmbraco9
         private readonly IWebHostEnvironment _env;
         private readonly IConfiguration _config;
 
+        // TEST
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Startup" /> class.
         /// </summary>
