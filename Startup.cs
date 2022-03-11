47def4f5fc5b3a868696d81c0fce80a9ca0a3f38
Odysseus Ribeiro <odysseus.ribeiro@hotmail.com>
2022-03-11 12:27:11 +0100

    Suppression du commentaire "Coucou" dans le startup.cs pour test

--- Startup.cs
+++ Startup.cs
@@ -13,10 +13,6 @@ namespace FormationCMSUmbraco9
     {
         private readonly IWebHostEnvironment _env;
         private readonly IConfiguration _config;
-        
-        
-        // Coucou
-
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Startup" /> class.
